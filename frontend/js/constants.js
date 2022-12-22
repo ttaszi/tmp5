9216b66626bb7e9f838a324ca6d7ade5d8c81164
ttaszi <116489332+ttaszi@users.noreply.github.com>
2022-12-22 22:14:40 +0100

    Update constants.js

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,7 +1,7 @@
 const contractAddress = "0xc6AaCCF9E47152a3A3AEe03d60E04B813DE79e42"; // Replace with your own contract address
 const chain = 'polygon'; // goerli, polygon, or ethereum
 
-const welcome_h1 = "Welcome to MyTrial6 NFT Project ?";
+const welcome_h1 = "Welcome to MyTrial6 NFT Project :)";
 const welcome_h2 = "Connect to MetaMask to Get Started";
 const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
