06cafb5b80a1aa294707a03136dc6f95850fdb0c
ttaszi <116489332+ttaszi@users.noreply.github.com>
2022-12-22 20:41:04 +0100

    Update app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -285,7 +285,7 @@ function setTotalPrice() {
     mintInput.disabled = true;
     return;
   }
-  const totalPriceWei = BigInt(info.deploymentConfig.publicMintPrice) * BigInt(mintInputValue);
+  const totalPriceWei = BigInt(price) * BigInt(mintInputValue);
   
   let priceType = '';
   if(chain === 'goerli' || chain === 'ethereum') {
