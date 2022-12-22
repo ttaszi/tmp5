073dc1c05c59d7ae8b2dc1067896e245dc8391ae
ttaszi <116489332+ttaszi@users.noreply.github.com>
2022-12-22 17:25:45 +0100

    Update app.js
    row 296 from 'ether' to 'MATIC'

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -293,7 +293,7 @@ function setTotalPrice() {
   } else if (chain === 'polygon') {
     priceType = 'MATIC';
   }
-  const price = web3.utils.fromWei(totalPriceWei.toString(), 'ether');
+  const price = web3.utils.fromWei(totalPriceWei.toString(), 'MATIC');
   totalPrice.innerText = `${price} ${priceType}`;
   mintButton.disabled = false;
   mintInput.disabled = false;
