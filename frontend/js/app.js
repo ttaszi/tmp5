8a3ce62c953e49b6bd88c95bb899d4b5df65e756
ttaszi <116489332+ttaszi@users.noreply.github.com>
2022-12-22 16:45:59 +0100

    Update app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -228,7 +228,7 @@ async function loadInfo() {
   } else if (chain === 'polygon') {
     priceType = 'MATIC';
   }
-  const price = web3.utils.fromWei(info.deploymentConfig.publicMintPrice);
+  const price = web3.utils.fromWei(info.deploymentConfig.publicMintPrice, 'MATIC');
   const pricePerMint = document.getElementById("pricePerMint");
   const maxPerMint = document.getElementById("maxPerMint");
   const totalSupply = document.getElementById("totalSupply");
