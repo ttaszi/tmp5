9066a2cab81de5e96d7cb37cdefe91c880b918d2
ttaszi <116489332+ttaszi@users.noreply.github.com>
2022-12-22 20:33:12 +0100

    Update app.js
    replace "web3.utils.fromWei(info.deploymentConfig.publicMintPrice, 'ether')" to 2

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -228,7 +228,7 @@ async function loadInfo() {
   } else if (chain === 'polygon') {
     priceType = 'MATIC';
   }
-  const price = web3.utils.fromWei(info.deploymentConfig.publicMintPrice, 'ether');
+  const price = 2;
   const pricePerMint = document.getElementById("pricePerMint");
   const maxPerMint = document.getElementById("maxPerMint");
   const totalSupply = document.getElementById("totalSupply");
