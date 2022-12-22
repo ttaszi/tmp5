c2dba802ea0fc45a799a72ce722f2e0899fefe62
ttaszi <116489332+ttaszi@users.noreply.github.com>
2022-12-22 17:31:26 +0100

    Update app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -293,7 +293,7 @@ function setTotalPrice() {
   } else if (chain === 'polygon') {
     priceType = 'MATIC';
   }
-  const price = web3.utils.fromWei(totalPriceWei.toString());
+  const price = web3.utils.fromWei(totalPriceWei.toString(), 'ether');
   totalPrice.innerText = `${price} ${priceType}`;
   mintButton.disabled = false;
   mintInput.disabled = false;
